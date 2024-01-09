eb398229ded545e23468e8141bac99f7949623e1
nastaziaf <nastasiapht@gmail.com>
2024-01-09 20:51:47 +0200

    add const variables

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -13,11 +13,15 @@
  *
  */
 
+// Initialize constant variables
+var type_function = 'function';
+var type_object = 'object';
+
 (function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
+  if (typeof define === type_function && define.amd) {
     // AMD. Register as an anonymous module.
     define(['superagent', 'querystring'], factory);
-  } else if (typeof module === 'object' && module.exports) {
+  } else if (typeof module === type_object && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
     module.exports = factory(require('superagent'), require('querystring'));
   } else {
